382fec68eaa866d06d5c75538659e25041b68e0e
Avsanka <nikita555534@gmail.com>
2022-03-29 22:19:22 +0700

    error fix

--- markup.py
+++ markup.py
@@ -1,5 +1,7 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
+btnBack = KeyboardButton('Back to main')
+
 btnMainEge = KeyboardButton('ЕГЭ')
 btnMainSam = KeyboardButton('Самостоятельное изучение')
 btnMainLanHelp = KeyboardButton('Помощь с выбором языка')
@@ -9,12 +11,11 @@ mainMenu = ReplyKeyboardMarkup(resize_keyboard=True).add(btnMainEge, btnMainSam,
 btnEgePascal = KeyboardButton('Pascal')
 btnEgePython = KeyboardButton('Python')
 btnEgeCpp = KeyboardButton('C++')
-btnBack = KeyboardButton('Back to main')
 egeEgeLanMenu = ReplyKeyboardMarkup(resize_keyboard=True).add(btnEgePascal, btnEgePython, btnEgeCpp, btnBack)
 
 btnSamCs = KeyboardButton('C#')
 btnSamCpp = KeyboardButton('C++')
-samMenu = ReplyKeyboardMarkup(resize_keyboard=True).add(btnSamCs,BtnSamCpp,btnBack)
+samMenu = ReplyKeyboardMarkup(resize_keyboard=True).add(btnSamCs, btnSamCpp, btnBack)
 
 lanHelpMenu = ReplyKeyboardMarkup(resize_keyboard=True).add(btnBack)
 
